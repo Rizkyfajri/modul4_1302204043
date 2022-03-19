601d5a32ec150f80d753c7674708e75f54e4bb51
Rizky Fajri <mansamussolini@gmail.com>
2022-03-19 16:26:28 +0700

    menambahkan class SimpleDataBase

--- modul4_1302204043/Program.cs
+++ modul4_1302204043/Program.cs
@@ -1,4 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 //Nim : 1302204043
 //NIM berakhiran 3, 4 atau 5: tipe data input double
+
+namespace modul4_1302204043
+{
+    public class Program
+    {
+        static void Main()
+        {
+            //Console.WriteLine("Masukan NIM : ");
+            //double A = Convert.ToDouble(Console.ReadLine());
+            //double B = Convert.ToDouble(Console.ReadLine());
+            //double C = Convert.ToDouble(Console.ReadLine());
+
+            double A = 13;//memberi isi variable
+            double B = 44;
+            double C = 43;
+
+            SimpleDataBase<double> DB = new SimpleDataBase<double>(); //inisialisasi SimpleDataBase ke DB
+
+            DB.AddNewData(A); //mengisi DB method AddNewData
+            DB.AddNewData(B);
+            DB.AddNewData(C);
+
+            DB.PrintAllData(); //meng print nilai yang disimpan
+        }
+    }
+}
---
+++ modul4_1302204043/SimpleDataBase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302204043
+{
+    public class SimpleDataBase<T>
+    {
+        List<T>? storedData;//data
+        List<DateTime>? inputDates;//tanggal
+        /*
+        public T? Data
+        {
+            get;
+            set;
+        }
+        */
+        public SimpleDataBase()//constructor
+        {
+            storedData = new List<T>();
+            inputDates = new List<DateTime>();
+
+
+        }
+        public void AddNewData(T data)  
+        {
+            storedData.Add(data);
+            inputDates.Add(DateTime.Now);
+
+            //PrintAllData();
+        }
+        public void PrintAllData()
+        {
+            //Console.WriteLine(storedData);
+            //Console.WriteLine(inputDates);
+
+            foreach (var data in storedData)
+            {
+                Console.WriteLine(data);//output data
+            }
+            foreach (var data in inputDates)
+            {
+                Console.WriteLine(data);//output tanggal
+            }
+        }
+    }
+}
