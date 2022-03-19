dec7ca371d08b79ec5a9d6b3c369c45a2b9b71cb
Rizky Fajri <mansamussolini@gmail.com>
2022-03-19 15:49:34 +0700

    4.MENAMBAHKAN METHOD DENGAN GENERIC

---
+++ modul4_1302204043/Penjumlahan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302204043
+{
+    internal class Penjumlahan
+    {
+        public void JumlahTigaAngka<T>(double A, double B, double C)
+        {
+            double Dig1 = A;
+            double Dig2 = B;
+            double Dig3 = C;
+
+            double output = A + B + C;
+            Console.WriteLine(output);
+        }
+    }
+}
--- modul4_1302204043/Program.cs
+++ modul4_1302204043/Program.cs
@@ -1,4 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 //Nim : 1302204043
 //NIM berakhiran 3, 4 atau 5: tipe data input double
+
+namespace modul4_1302204043
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Masukan NIM : ");
+            double A = Convert.ToDouble(Console.ReadLine());
+            double B = Convert.ToDouble(Console.ReadLine());
+            double C = Convert.ToDouble(Console.ReadLine());
+
+            Penjumlahan jum = new Penjumlahan();
+
+            jum.JumlahTigaAngka<double>(A, B, C);
+        }
+    }
+}
